36fe45dbb0efa4e3903de1c1d2a341065df876ed
DONDON <abesamislyndon@gmail.com>
2015-08-18 17:47:59 +0800

    add ngDialog for read more onclick

--- js/main.js
+++ js/main.js
@@ -1,30 +1,34 @@
 /**
  * Main AngularJS Web Application
  */
-var app = angular.module('tutorialWebApp', ['ngRoute']);
+var app = angular.module('tutorialWebApp', ['ngRoute', 'ngDialog']);
 
-/**
- * Configure the Routes
- */
-app.config(['$routeProvider', function ($routeProvider, $locationProvider) {
+app.config(['$routeProvider','$locationProvider','ngDialogProvider' ,function ($routeProvider, $locationProvider, ngDialogProvider) {
+
   $routeProvider
-    // Home
     .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
-    // Pages
     .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
     .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
     .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
     .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
     .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
-    // Blog
     .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
     .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
-    // else 404
     .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 
+        ngDialogProvider.setDefaults({
+                className: 'ngdialog-theme-default',
+                plain: false,
+                showClose: true,
+                closeByDocument: true,
+                closeByEscape: true,
+                appendTo: false,
+                preCloseCallback: function () {
+                    console.log('default pre-close callback');
+                }
+            });
+
 }]);
-
-
 
 /**
  * Controls the Blog
@@ -49,3 +53,44 @@ app.controller('PageCtrl', function (/* $scope, $location, $http */) {
     selector: "a[data-toggle=tooltip]"
   })
 });
+
+  app.controller('MainCtrl', function ($scope, $rootScope, ngDialog, $timeout) {
+      $rootScope.jsonData = '{"foo": "bar"}';
+      $rootScope.theme = 'ngdialog-theme-default';
+
+ 
+
+   $scope.openDefault = function () {
+                ngDialog.open({
+                    template: 'firstDialogId',
+                    controller: 'InsideCtrl',
+                    className: 'ngdialog-theme-default'
+                });
+            };
+
+
+    });
+
+       app.controller('InsideCtrl', function ($scope, ngDialog) {
+            $scope.dialogModel = {
+                message : 'Does it faze you, just thinking about what does it mean? Here’s a short way of thinking about it – the publisher would be looking into making your site an easier find through the maze of the internet. Does it faze you, just thinking about what does it mean? Here’s a short way of thinking about it – the publisher would be looking into making your site an easier find through the maze of the internet.'
+            };
+            $scope.openSecond = function () {
+                ngDialog.open({
+                    template: '<h3><a href="" ng-click="closeSecond()">Close all by click here!</a></h3>',
+                    plain: true,
+                    closeByEscape: false,
+                    controller: 'SecondModalCtrl'
+                });
+            };
+        });
+
+        app.controller('InsideCtrlAs', function () {
+            this.value = 'value from controller';
+        });
+
+        app.controller('SecondModalCtrl', function ($scope, ngDialog) {
+            $scope.closeSecond = function () {
+                ngDialog.close();
+            };
+        });
